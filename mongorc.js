699f347495b123aaffccbd22ea0913c664c248cb
Dan Crosta <dcrosta@late.am>
2012-03-02 23:18:48 -0500

    track .mongorc.js

---
+++ mongorc.js
@@ -0,0 +1,21 @@
+prompt = function() {
+  var serverStatus = db.serverStatus();
+
+  var hostname = serverStatus.host;
+  if (!/\d+\.\d+\.\d+\.\d+/.test(hostname) && /\./.test(hostname))
+    hostname = hostname.substr(0, hostname.indexOf('.'));
+
+  if (serverStatus.process != "mongod") {
+    return serverStatus.process + '@' + hostname + ':' + db + '> ';
+  } else {
+    var states = ["startup", "primary", "uecondary", "recovering", "fatal", "startup2", "unknown", "arbiter", "down", "rollback"];
+    var rsstatus = db.adminCommand({replSetGetStatus: 1});
+
+    if (rsstatus.ok == 0) {
+      return hostname + ':' + db + '> ';
+    } else {
+      return states[rsstatus.myState] + '@' + hostname + ':' + db + '> ';
+    }
+  }
+}
+
