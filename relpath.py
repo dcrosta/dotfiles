41e9695be00f1fa1c5943966bfcb06a021468141
Dan Crosta <dcrosta@late.am>
2018-01-22 16:20:00 -0500

    don't leave scary errors on the terminal

--- relpath.py
+++ relpath.py
@@ -3,5 +3,9 @@ import sys
 
 relative_to = sys.argv[1]
 
-for line in sys.stdin:
-    print(os.path.relpath(line.strip(), relative_to))
+try:
+    for line in sys.stdin:
+        print(os.path.relpath(line.strip(), relative_to))
+except IOError:
+    # broken pipe, usually, just ignore
+    pass
