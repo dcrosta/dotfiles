c509626537ee3e77ef84744982c1e32bd2a6144e
Dan Crosta <dcrosta@dropbox.com>
2020-04-22 17:28:33 +0000

    make it easier for dotfiles/userfiles sync

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ rm -rf dotfiles-backup
 mkdir -p dotfiles-backup
 
 # deploy
-for f in ackrc vimrc gitconfig gitignore zshrc zsh vim mongorc.js pythonrc gemrc
+for f in $(cat DOTFILES)
 do
     if [ -e ~/.${f} ]; then
         mv ~/.${f} dotfiles-backup/${f}
